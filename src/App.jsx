512c0d43fbd78d5f3c6a8d004d09652b7026dbb4
LuigiCastillo <rubato.7486@gmail.com>
2024-03-12 10:43:57 -0600

    Se hace la peticion a la API coincap al DOM de este proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react"
+
 function App() {
+
+  const [criptos, setCriptos] = useState()
+
+  useEffect( () => {
+    fetch("https://api.coincap.io/v2/assets")
+    .then((resp) => resp.json())
+    .then((data) => {
+      setCriptos(data.data)
+    })
+    .catch(() => {
+      console.error("La peticion fallo")
+    })
+  }, [])
+
+  if (!criptos) return <span>Cargando...</span>
 
   return (
     <>
-    <h1>Hola mundo</h1>
+      <h1>Lista de criptomonedas</h1>
+      <ol>
+        { criptos.map(({name, priceUsd}) => (
+          <li>Nombre: {name} Precio: {priceUsd}</li>
+        )) }
+      </ol>
     </>
   )
 }
