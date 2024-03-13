e5182051afeca6c5c0be57722bbbfee4d9775f2d
LuigiCastillo <rubato.7486@gmail.com>
2024-03-13 09:54:46 -0600

    Se da estilos a DOM -Lista de criptomonedas-

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
 import "./App.css"
 import Cripto from "./componentes/Cripto"
 
+
+
 function App() {
+
+  const API_URL = import.meta.env.VITE_API_URL
+  const [criptos, setCriptos] = useState()
+
+  useEffect( () => {
+      axios.get(`${API_URL}assets`)
+      .then((data) => {
+        setCriptos(data.data.data)
+      })
+      .catch(() => {
+        console.error("La peticion fallo")
+      })
+    }, [])
+  
+    if (!criptos) return <span>Cargando...</span>
+
   return (
-    <>
+    <div className="app-container">
       <h1>Lista de criptomonedas</h1>
-      <Cripto/>
-    </>
+      <div className="cripto-container">
+        {
+          criptos.map(({id, name, priceUsd, symbol, changePercent24Hr}) => (
+            <Cripto key={id} name={name} priceUsd={priceUsd} symbol={symbol} changePercent24Hr={changePercent24Hr}/>
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
--- src/componentes/Cripto.jsx
+++ src/componentes/Cripto.jsx
@@ -1,34 +1,21 @@
-import { useEffect, useState } from "react"
-import axios from "axios"
+import "../Cripto.css"
 
-function Cripto () {
-
-    const API_URL = import.meta.env.VITE_API_URL
-    const [criptos, setCriptos] = useState()
-
-    useEffect( () => {
-        axios.get(`${API_URL}assets`)
-        .then((data) => {
-          setCriptos(data.data.data)
-        })
-        .catch(() => {
-          console.error("La peticion fallo")
-        })
-      }, [])
-    
-      if (!criptos) return <span>Cargando...</span>
+const Cripto = ({name, priceUsd, symbol, changePercent24Hr}) => {
 
     return (
-        <>
-            { 
-                criptos.map(({name, priceUsd, id}) => (
-                    <div key={id} className="Card">
-                        <p>Nombre: {name}</p>
-                        <p>Precios: {priceUsd}</p>
-                    </div>
-                ))
-            }
-      </>
+        <div className="cripto">
+            <h2>{name}</h2>
+            <div className="info">
+                <p><span className="label">Precio: </span>{parseFloat(priceUsd).toFixed(4)}</p>
+                <p><span className="label">Codigo: </span>{symbol}</p>
+                <p>
+                    <span className="label">Variacion 24 hrs: </span>
+                    <span className={ parseFloat(changePercent24Hr) > 0 ? "positivo" : "negativo"}>{parseFloat(changePercent24Hr).toFixed(4)}</span>
+                </p>
+
+            </div>
+
+        </div>
     )
 }
 
