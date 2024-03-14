e45f6a5a21ac35912336452df11cc2c8e4943466
LuigiCastillo <rubato.7486@gmail.com>
2024-03-14 17:04:41 -0600

    Se añade 'datos de criptomoneda' e 'historial' a la pagina -CriptoPage-

--- src/componentes/Cripto/CriptoPage.jsx
+++ src/componentes/Cripto/CriptoPage.jsx
@@ -1,15 +1,105 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
 import { useParams } from "react-router-dom"
+import { key } from "localforage"
 
-const CriptoPAge = () => {
+const CriptoPage = () => {
 
     const params = useParams()
+    const API_URL = import.meta.env.VITE_API_URL
+    const [criptos, setCriptos] = useState()
+    const [history, setHistory] = useState([])
+
+
+    useEffect( () => {
+        axios.get(`${API_URL}assets/${params.id}`)
+        .then((data) => {
+          setCriptos(data.data.data)
+        })
+        .catch(() => {
+          console.error("La peticion fallo")
+        })
+      }, [])
+  
+    useEffect( () => {
+        axios.get(`${API_URL}assets/${params.id}/history?interval=d1`)
+        .then((data) => {
+          setHistory(data.data.data)
+        })
+        .catch(() => {
+          console.error("La peticion fallo")
+        })
+      }, [])
+    
+      if (!criptos) return <span>Cargando...</span>
 
     return (
-        <h1>Soy la criptomoneda {params.id}</h1>
-        // Colocar la informacion de la criptomoneda seleccionada
-        // Dar estilos
-        
+        <>
+            <h1>Datos de la criptomoneda {params.id}</h1>
+            <div className="criptopage-container">
+                <ul>
+                    {/* <li><span className="label">Key: </span>{criptos.key}</li> */}
+                    {/* <li><span className="label">Nombre: </span>{criptos.name}</li> */}
+                    {/* <li><span className="label">Nombre: </span>{criptos.marketCapUsd}</li> */}
+                    {/* <li><span className="label">Nombre: </span>{criptos.volumeUsd24Hr}</li> */}
+                    <li>
+                        <span className="label">Nombre: </span>
+                        <span>{criptos.id}</span>
+                    </li>
+                    <li>
+                        <span className="label">Rango: </span>
+                        <span>{criptos.rank}</span>
+                    </li>
+                    <li>
+                        <span className="label">Simbolo: </span>
+                        <span>{criptos.symbol}</span>
+                    </li>
+                    
+                    <li>
+                        <span className="label">En circulación:</span>
+                        <span>{parseFloat(criptos.supply).toFixed(4)}</span>
+                    </li>
+                    <li>
+                        <span className="label">Suministro maximo: </span>
+                        <span>{parseFloat(criptos.maxSupply).toFixed(4)}</span>
+                    </li>
+                    
+                    <li>
+                        <span className="label">Precio en dolares: </span>
+                        <span>{parseFloat(criptos.priceUsd).toFixed(4)}</span>
+                    </li>
+                    <li>
+                        <span className="label">Cambio en las ultimas 24 horas: </span>
+                        <span>{parseFloat(criptos.changePercent24Hr).toFixed(4)}</span>
+                    </li>
+                    <li>
+                        <span className="label">Nombre: </span>
+                        <span>{parseFloat(criptos.vwap24Hr).toFixed(4)}</span>
+                    </li>
+                </ul>
+            </div>
+            <h2>Historial</h2>
+            {/* <span>{JSON.stringify(history)}</span> */}
+            <table>
+                <thead>
+                    <tr>
+                        <th>Fecha</th>
+                        <th>Precio</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        history.map(({ date, priceUsd, time}) => (
+                            <tr key={time}>
+                                <td>{date}</td>
+                                <td>{parseFloat(priceUsd).toFixed(4)}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
+        </>
     )
 }
 
-export default CriptoPAge
+export default CriptoPage
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Pagina404 from './componentes/404.jsx'
 import Cuadricula from './componentes/Cuadricula.jsx'
 import Home from './Home.jsx'
-import CriptoPAge from './componentes/Cripto/CriptoPage.jsx'
+import CriptoPage from './componentes/Cripto/CriptoPage.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -17,7 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             </Route>
             <Route path='/criptomonedas' element={<App/>}>
                 <Route index element={<Cuadricula />}/>
-                <Route path=':id' element={<CriptoPAge/>}/>
+                <Route path=':id' element={<CriptoPage/>}/>
             </Route>
             <Route path='*' element={<Pagina404/>}/>
         </Routes>
