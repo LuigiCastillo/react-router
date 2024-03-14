6efbf97ed1756db92c354b3bf12368e7f0c636b6
LuigiCastillo <rubato.7486@gmail.com>
2024-03-14 10:16:37 -0600

    Se crea barra de navegacion con componente -outlet-

--- src/componentes/App.jsx
+++ src/componentes/App.jsx
@@ -1,39 +1,13 @@
-import { useEffect, useState } from "react"
-import axios from "axios"
-import "./App.css"
-import Cripto from "./Cripto"
+import { Outlet } from "react-router-dom"
+import Menu from "./menu/Menu"
 
-
-
-function App() {
-
-  const API_URL = import.meta.env.VITE_API_URL
-  const [criptos, setCriptos] = useState()
-
-  useEffect( () => {
-      axios.get(`${API_URL}assets`)
-      .then((data) => {
-        setCriptos(data.data.data)
-      })
-      .catch(() => {
-        console.error("La peticion fallo")
-      })
-    }, [])
-  
-    if (!criptos) return <span>Cargando...</span>
-
-  return (
-    <div className="app-container">
-      <h1>Lista de criptomonedas</h1>
-      <div className="cripto-container">
-        {
-          criptos.map(({id, name, priceUsd, symbol, changePercent24Hr}) => (
-            <Cripto key={id} name={name} priceUsd={priceUsd} symbol={symbol} changePercent24Hr={changePercent24Hr}/>
-          ))
-        }
-      </div>
-    </div>
-  )
+const App = () => {
+    return (
+        <>
+            <Menu/>
+            <Outlet/>
+        </>
+    )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,17 @@ import "./main.css"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import CONTACT from './componentes/Contact.jsx'
 import Pagina404 from './componentes/404.jsx'
-import Menu from './componentes/menu/Menu.jsx'
+import Cuadricula from './componentes/Cuadricula.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <>
-        <BrowserRouter>
-        <Menu />
-            <Routes>
-                <Route path="/" element={<App />}/>
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<App/>}>
+                <Route index element={<Cuadricula />}/>
                 <Route path="/contact" element={<CONTACT />}/>
-                <Route path='*' element={<Pagina404/>}/>
-            </Routes>
-        </BrowserRouter>
-    </>
+            </Route>
+            <Route path='*' element={<Pagina404/>}/>
+        </Routes>
+    </BrowserRouter>
 )
