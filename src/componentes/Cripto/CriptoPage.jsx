d11c8eaefea7595f94413b808f59944da8f105fa
LuigiCastillo <rubato.7486@gmail.com>
2024-03-15 10:58:18 -0600

    Se da estilos a -Home, cripto, criptopage y menu-

--- src/componentes/Cripto/CriptoPage.jsx
+++ src/componentes/Cripto/CriptoPage.jsx
@@ -21,9 +21,7 @@ const CriptoPage = () => {
         .catch(() => {
           console.error("La peticion fallo")
         })
-      }, [])
-  
-    useEffect( () => {
+
         axios.get(`${API_URL}assets/${params.id}/history?interval=d1`)
         .then((data) => {
           setHistory(data.data.data)
@@ -37,69 +35,61 @@ const CriptoPage = () => {
 
     return (
         <>
-            <h1>Datos de la criptomoneda {params.id}</h1>
-            <div className="criptopage-container">
-                <ul>
-                    {/* <li><span className="label">Key: </span>{criptos.key}</li> */}
-                    {/* <li><span className="label">Nombre: </span>{criptos.name}</li> */}
-                    {/* <li><span className="label">Nombre: </span>{criptos.marketCapUsd}</li> */}
-                    {/* <li><span className="label">Nombre: </span>{criptos.volumeUsd24Hr}</li> */}
-                    <li>
-                        <span className="label">Nombre: </span>
-                        <span>{criptos.id}</span>
-                    </li>
-                    <li>
-                        <span className="label">Rango: </span>
-                        <span>{criptos.rank}</span>
-                    </li>
-                    <li>
-                        <span className="label">Simbolo: </span>
-                        <span>{criptos.symbol}</span>
-                    </li>
-                    
-                    <li>
-                        <span className="label">En circulación:</span>
-                        <span>{parseFloat(criptos.supply).toFixed(4)}</span>
-                    </li>
-                    <li>
-                        <span className="label">Suministro maximo: </span>
-                        <span>{parseFloat(criptos.maxSupply).toFixed(4)}</span>
-                    </li>
-                    
-                    <li>
-                        <span className="label">Precio en dolares: </span>
-                        <span>{parseFloat(criptos.priceUsd).toFixed(4)}</span>
-                    </li>
-                    <li>
-                        <span className="label">Cambio en las ultimas 24 horas: </span>
-                        <span>{parseFloat(criptos.changePercent24Hr).toFixed(4)}</span>
-                    </li>
-                    <li>
-                        <span className="label">Nombre: </span>
-                        <span>{parseFloat(criptos.vwap24Hr).toFixed(4)}</span>
-                    </li>
-                </ul>
-            </div>
-            <h2>Historial</h2>
-            {/* <span>{JSON.stringify(history)}</span> */}
-            <table>
-                <thead>
-                    <tr>
-                        <th>Fecha</th>
-                        <th>Precio</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        history.map(({ date, priceUsd, time}) => (
-                            <tr key={time}>
-                                <td>{dayjs(date).format(`DD/MM/YYYY`)}</td>
-                                <td>{parseFloat(priceUsd).toFixed(4)}</td>
+            <div className="cripto-page-container">
+                {/* Informacion */}
+                <div className="info">
+                    {/* Informacion principal */}
+                    <div className="main-info">
+                        <span>Ranking: {criptos.rank}</span>
+                        <h1>{criptos.name}</h1>
+                        <span className="symbol">Simbolo: {criptos.symbol}</span>
+                    </div>
+                    {/* Informacion detallada */}
+                    <div className="details">
+                        <ul>
+                            <li className="detail">
+                                <span className="label">Precio en dolares: </span>
+                                <span>{parseFloat(criptos.priceUsd).toFixed(4)}</span>
+                            </li>
+                            <li className="detail">
+                                <span className="label">En circulación:</span>
+                                <span>{parseFloat(criptos.supply).toFixed(4)}</span>
+                            </li>
+                            <li className="detail">
+                                <span className="label">Suministro maximo: </span>
+                                <span>{parseFloat(criptos.maxSupply).toFixed(4)}</span>
+                            </li>
+                            <li className="detail">
+                                <span className="label">Variación: </span>
+                                <span>{parseFloat(criptos.changePercent24Hr).toFixed(4)}</span>
+                            </li>
+                            <li className="detail">
+                                <span className="label">vwap24Hr: </span>
+                                <span>{parseFloat(criptos.vwap24Hr).toFixed(4)}</span>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                {/* Historial */}
+                <div className="history">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Fecha</th>
+                                <th>Precio</th>
                             </tr>
-                        ))
-                    }
-                </tbody>
-            </table>
+                        </thead>
+                        <tbody>
+                            {history.map(({ date, priceUsd, time}) => (
+                                <tr key={time}>
+                                    <td>{dayjs(date).format(`DD/MM/YYYY`)}</td>
+                                    <td>{parseFloat(priceUsd).toFixed(3)}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </>
     )
 }
