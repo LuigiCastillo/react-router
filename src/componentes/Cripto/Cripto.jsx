92c315ca32671fdcbddf578ccd37569ea32878cc
LuigiCastillo <rubato.7486@gmail.com>
2024-03-15 09:56:46 -0600

    Se le da formato a historial de criptomoneda individual

--- src/componentes/Cripto/Cripto.jsx
+++ src/componentes/Cripto/Cripto.jsx
@@ -10,8 +10,12 @@ const Cripto = ({id, name, priceUsd, symbol, changePercent24Hr}) => {
                 <p><span className="label">Precio: </span>{parseFloat(priceUsd).toFixed(4)}</p>
                 <p><span className="label">Codigo: </span>{symbol}</p>
                 <p>
-                    <span className="label">Variacion 24 hrs: </span>
-                    <span className={ parseFloat(changePercent24Hr) > 0 ? "positivo" : "negativo"}>{parseFloat(changePercent24Hr).toFixed(4)}</span>
+                    <span className="label">
+                        Variacion 24 hrs: 
+                    </span>
+                    <span className={ parseFloat(changePercent24Hr) > 0 ? "positivo" : "negativo"}>
+                        {parseFloat(changePercent24Hr).toFixed(4)}
+                    </span>
                 </p>
                 <Link to={`/criptomonedas/${id}`}>Ver detalles</Link>
             </div>
