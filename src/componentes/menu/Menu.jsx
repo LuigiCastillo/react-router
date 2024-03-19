bb3ed2517269c0d0234e5dd2c5d2d2f57c1821ff
LuigiCastillo <rubato.7486@gmail.com>
2024-03-19 07:45:55 -0600

    Se usa el hook -createContext y useContext- data compartida

--- src/componentes/menu/Menu.jsx
+++ src/componentes/menu/Menu.jsx
@@ -1,13 +1,21 @@
 import { NavLink } from "react-router-dom"
 import "./Menu.css"
+import { UserContext } from "../../context/UserContext"
+import { useContext } from "react"
 
-const Menu = () => (
+const Menu = () => {
+
+    const usuario = useContext(UserContext)
+
+    return (
     <nav className="main-menu">
         <ul>
             <li><NavLink to="/">Inicio</NavLink> </li>
             <li><NavLink to="/criptomonedas">Lista de criptos</NavLink> </li>
+            <li><NavLink to="/perfil">Perfil de {usuario.name}</NavLink></li>
         </ul>
     </nav>
-)
+    )
+}
 
 export default Menu
--- src/main.jsx
+++ src/main.jsx
@@ -7,19 +7,25 @@ import Pagina404 from './componentes/404.jsx'
 import Cuadricula from './componentes/Cuadricula.jsx'
 import Home from './Home.jsx'
 import CriptoPage from './componentes/Cripto/CriptoPage.jsx'
+import Perfil from './perfil.jsx'
+import { UserContextProvider } from './context/UserContext.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<App/>}>
-                <Route index element={<Home />}/>
-            </Route>
-            <Route path='/criptomonedas' element={<App/>}>
-                <Route index element={<Cuadricula />}/>
-                <Route path=':id' element={<CriptoPage/>}/>
-            </Route>
-            <Route path='*' element={<Pagina404/>}/>
-        </Routes>
-    </BrowserRouter>
+    <UserContextProvider>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<App/>}>
+                    <Route index element={<Home />}/>
+                    <Route path='/perfil' element={<Perfil/>}/>
+                </Route>
+                <Route path='/criptomonedas' element={<App/>}>
+                    <Route index element={<Cuadricula />}/>
+                    <Route path=':id' element={<CriptoPage/>}/>
+                </Route>
+                <Route path='*' element={<Pagina404/>}/>
+            </Routes>
+        </BrowserRouter>
+    </UserContextProvider>
+    
 )
